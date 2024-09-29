9f3e9c447226d12b6156c55e6e07dcf5299c0da6
Your Name <pasapera279@gmail.com>
2024-09-28 22:27:50 -0500

    week01

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,34 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.Write("What is your magic number?");
-        // int magicNumber = int.Parse(Console.ReadLine());
-
         Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,107);
+        int magicNumber = randomGenerator.Next(1, 10);
 
         int guess = -1;
-
         do
         {
-            Console.Write("What is your guess?");
+            Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
 
-        } while (magicNumber != guess);
-        {
-            if(magicNumber > guess)
+            if (magicNumber > guess)
             {
                 Console.WriteLine("Higher");
             }
-            else if(magicNumber < guess)
+            else if (magicNumber < guess)
             {
                 Console.WriteLine("Lower");
             }
             else
             {
-                Console.WriteLine("You guess it! :)");
+                Console.WriteLine("You guessed it! :)");
             }
-        }
-    
+
+        } while (magicNumber != guess); 
     }
 }
